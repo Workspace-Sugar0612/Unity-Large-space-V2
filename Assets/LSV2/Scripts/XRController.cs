4cc09dd0ce31daf7a73828ba59d067fe4343f3ee
Sugar <287276293@qq.com>
2025-01-24 14:27:18 +0800

    chore: Init project.

---
+++ Assets/LSV2/Scripts/XRController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.PXR;
+using UnityEngine;
+
+public class XRController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        PXR_Manager.EnableVideoSeeThrough = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            PXR_Manager.EnableVideoSeeThrough = true;
+        }
+    }
+}
